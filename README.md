5db46320cdd29c9d59a0f03374b022f6ff2d9a90
Hank Stocker <email@example.com>
2020-01-29 09:57:59 -0600

    Get project to build with minimal config

--- README.md
+++ README.md
@@ -10,6 +10,16 @@ This crate is currently in the early development stage and should not be used fo
 
 [Documentation](https://docs.rs/plcnext).
 
-## PLCnext Community
+## Building
 
-Please share your experiences with the [PLCnext Community](https://plcnext-community.net), in the [Makers Blog](https://www.plcnext-community.net/index.php?option=com_content&view=category&layout=blog&id=78&Itemid=365&lang=en) or in the [Public Forum](https://www.plcnext-community.net/index.php?option=com_easydiscuss&view=categories&Itemid=221&lang=en) 
+To build the project, you'll need to export the following three vars; for WSL1, append these to your `.bashrc`.
+
+```
+export PATH="$PATH:/opt/pxc/2020.0/sysroots/x86_64-pokysdk-linux/usr/bin:/opt/pxc/2020.0/sysroots/x86_64-pokysdk-linux/usr/sbin:/opt/pxc/2020.0/sysroots/x86_64-pokysdk-linux/bin:/opt/pxc/2020.0/sysroots/x86_64-pokysdk-linux/sbin:/opt/pxc/2020.0/sysroots/x86_64-pokysdk-linux/usr/bin/../x86_64-pokysdk-linux/bin:/opt/pxc/2020.0/sysroots/x86_64-pokysdk-linux/usr/bin/arm-pxc-linux-gnueabi:/opt/pxc/2020.0/sysroots/x86_64-pokysdk-linux/usr/bin/arm-pxc-linux-musl"
+
+export BINDGEN_EXTRA_CLANG_ARGS="-target arm-pxc-linux-gnueabi --sysroot=/opt/pxc/2020.0 -I/opt/pxc/2020.0/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi/usr/include/plcnext"
+
+export PLCNEXT_HEADERS="-I/opt/pxc/2020.0/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi/usr/include/plcnext"
+```
+
+After that, you should be able to build normally via `cargo build --target=armv7-unknown-linux-gnueabihf`.
--- plcnext-sys/build/main.rs
+++ plcnext-sys/build/main.rs
@@ -3,11 +3,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-  let axc_2152_default_sysroot = String::from("/opt/pxc/2020.0/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi");
-  let _includes =
-    "/opt/pxc/2020.0/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi/usr/include/plcnext";
-
-  let env_sysroot = PathBuf::from(env::var("PLCNEXT_TARGET_SYSROOT").unwrap_or(axc_2152_default_sysroot));
+  let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
   // // Tell cargo to tell rustc to link the PLCnext ANSI-C libraries.
   println!("cargo:rustc-link-lib=Arp.System.Commons");
@@ -15,48 +11,11 @@ fn main() {
   println!("cargo:rustc-link-lib=Arp.System.Module");
   println!("cargo:rustc-link-lib=Arp.System.Rsc");
   println!("cargo:rustc-link-lib=Arp.Plc.AnsiC");
-  // println!("cargo:rustc-link-lib=stdc++");
-  let headers = "
-        #include \"Arp/Plc/AnsiC/ArpPlc.h\"
-        #include \"Arp/Plc/AnsiC/Device/Device.h\"
-        #include \"Arp/Plc/AnsiC/Domain/PlcManager.h\"
-        #include \"Arp/Plc/AnsiC/Domain/PlcOperation.h\"
-        #include \"Arp/Plc/AnsiC/Domain/PlcOperationHandler.h\"
-        #include \"Arp/Plc/AnsiC/Gds/DataLayout.h\"
-        #include \"Arp/Plc/AnsiC/Gds/GdsBuffer.h\"
-        #include \"Arp/Plc/AnsiC/Io/Axio.h\"
-        #include \"Arp/Plc/AnsiC/Io/FbIoSystem.h\"";
 
   let builder = bindgen::Builder::default()
     .header("include/wrapper.h")
-    // .header_contents("wrap.h", headers)
-    .clang_arg("--target=arm-pxc-linux-gnueabi")
-    .clang_arg("--sysroot=/opt/pxc/2020.0")
-    .clang_arg("-march=armv7-a")
-    .clang_arg("-mthumb")
-    .clang_arg("-mfpu=neon")
-    .clang_arg("-mfloat-abi=hard")
-    .clang_arg("-mcpu=cortex-a9")
-    .clang_arg("-I/opt/pxc/2020.0/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi/usr/include/plcnext")
-    .clang_arg("-v");
-  // .clang_args(vec!("-target arm-pxc-linux-gnueabi", "--sysroot=/opt/pxc/2020.0", "-I/opt/pxc/2020.0/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi/usr/include/plcnext"));
-  // .clang_arg("--include-directory=/usr/lib/llvm-8/lib/clang/8.0.0/include")
-
-  // export BINDGEN_EXTRA_CLANG_ARGS="-target arm-pxc-linux-gnueabi --sysroot=/opt/pxc/2020.0 -I/opt/pxc/2020.0/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi/usr/include/plcnext"
-
-  // export BINDGEN_EXTRA_CLANG_ARGS="-I/opt/pxc/2020.0/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi/usr/include/plcnext"
-
-  // Finish the builder and generate the bindings.
-  let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-  let flags = builder.command_line_flags();
-
-  println!("HEY MAN");
-  println!("{:#?}", flags);
-
-  let bindings = builder.generate().expect("Unable to generate bindings");
-
-  bindings
+    .generate()
+    .expect("Couldn't generate bindings!")
     .write_to_file(out_path.join("bindings.rs"))
-    .expect("Unable to write bindings to file");
+    .expect("Couldn't write bindings!");
 }
